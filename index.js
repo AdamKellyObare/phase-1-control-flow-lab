2d50c07d7540a12b8f7dd1885b5263c84da00e5f
adamKellyObare <aobarekelly@gmail.com>
2023-06-18 11:43:50 +0300

    Assist

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+  let distance;{
+    if (distance<400) {
+      return 'This one is on me'}
+    else if (distance>2500) {
+      return 'No can do'}
+    else if (distance>2000) {
+        return 'I will gladly take your thirty'}
+  }
+};
+
+    // tests continue..});
+
 
 function ternaryCheckCity(){
   // Write your code here!
-}
+  let city; {
+    if (city ='NYC' ) 
+    return 'Ok, sounds good.' 
+    else (city ='Pittsburgh') 
+    return 'No,go.'
+    
+  }
+
+};
 
 function switchOnCharmFromTip(){
   // Write your code here!
-}
+  let tip;  {
+    if (tip = 'generous') {
+return "Thank you so much.";}
+    else if (tip = 'not generous'){
+    return "Thank you.";}
+    else {
+      console.log ("Bye")
+    }
+  }
+
+  }
+ 
